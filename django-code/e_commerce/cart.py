48eb37ce92e2757df5c37e6db3aed7d729b99ea1
vodaiphuoc <phuocvodn98@gmail.com>
2025-11-27 04:13:45 +0700

    add typing

--- django-code/e_commerce/cart.py
+++ django-code/e_commerce/cart.py
@@ -1,6 +1,13 @@
 from decimal import Decimal
 from django.conf import settings
 from .models import Product
+from typing import TypedDict, Iterator
+
+class CartItem(TypedDict):
+    quantity: int
+    price: int
+    product: Product
+    total_price: int
 
 class Cart:
     def __init__(self, request):
@@ -10,27 +17,24 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, override_quantity=False):
+    def add(self, product: Product, quantity=1):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0, 'price': product.price}
         
-        if override_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
+        self.cart[product_id]['quantity'] += quantity
         self.save()
 
     def save(self):
         self.session.modified = True
 
-    def remove(self, product):
+    def remove(self, product: Product):
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
 
-    def __iter__(self):
+    def __iter__(self)-> Iterator[CartItem]:
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
