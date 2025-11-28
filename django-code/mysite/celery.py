b34e7ad7ba15e9fdc108d0d4e86481b709d58435
vodaiphuoc <phuocvodn98@gmail.com>
2025-11-28 20:23:57 +0700

    add celery config object

--- django-code/mysite/celery.py
+++ django-code/mysite/celery.py
@@ -1,10 +1,23 @@
 import os
 from celery import Celery
+from pathlib import Path
+from dotenv import load_dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 app = Celery('mysite')
 
-app.config_from_object('django.conf.settings', namespace='CELERY')
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".celery.env"))
 
+class CelerySettings:
+    broker_port=os.environ['BROKER_PORT']
+    broker_url=os.environ['BROKER_URL']
+    result_backend=os.environ['RESULT_BACKEND']
+    accept_content=[os.environ['ACCEPT_CONTENT']]
+    task_serializer=os.environ['TASK_SERIALIZER']
+    result_serializer=os.environ['RESULT_SERIALIZER']
+    timezone=os.environ['TIMEZONE']
+
+app.config_from_object(CelerySettings)
 app.autodiscover_tasks()
