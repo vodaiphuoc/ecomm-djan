67e23c049ddbc23c8a2b3ab8c1c6276eeefc4b01
vodaiphuoc <phuocvodn98@gmail.com>
2025-11-28 20:22:22 +0700

    enable signals

--- django-code/e_commerce/apps.py
+++ django-code/e_commerce/apps.py
@@ -5,5 +5,5 @@ class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'e_commerce'
 
-    # def ready(self):
-    #     import e_commerce.signals
+    def ready(self):
+        import e_commerce.signals
