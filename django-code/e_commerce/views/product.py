b051a97ec88803d8e9a61f0b257abca754a73432
vodaiphuoc <phuocvodn98@gmail.com>
2025-12-01 14:26:50 +0700

    add price filter on backend

--- django-code/e_commerce/views/product.py
+++ django-code/e_commerce/views/product.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.cache import cache_control
 from django.http import HttpRequest
 from django.core.paginator import Paginator
 from django.db.models import Q, Prefetch
+from django.contrib import messages
 
 TEMPLATE_FOLDER_NAME = 'e_commerce'
 
 from e_commerce.forms import ReviewForm
 from e_commerce.models import Product, Order, Category
 
-def product_list(request: HttpRequest, category_slug:str = None, category_id: int = None):
+def product_list(
+        request: HttpRequest, 
+        category_slug:str = None, 
+        category_id: int = None
+    ):
     
     parent_categories = Category.objects.filter(parent__isnull=True).order_by('name')
     categories_tree = parent_categories.prefetch_related(
@@ -17,23 +22,45 @@ def product_list(request: HttpRequest, category_slug:str = None, category_id: in
     )
     query = None
 
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+
+    if price_min is not None and price_max is not None:
+        try:
+            price_min = int(price_min)
+            price_max = int(price_max)
+            assert price_max > price_min
+            print(price_min, price_max)
+        except Exception as e:
+            messages.error(request, "giá trị min max không hợp lệ")
+            redirect(request.path)
+
     if category_slug is not None and category_id is not None:
         target_parent_cate = categories_tree.filter(id=category_id).first()
 
         if target_parent_cate:
             sub_categories = target_parent_cate.subcategories
             products = []
-            for sub_cate in sub_categories:
-                products.extend(sub_cate.products.all())
+            if price_min is not None and price_max is not None:
+                for sub_cate in sub_categories:
+                    products_queryset = sub_cate.products.filter(price__gte=price_min).filter(price__lte=price_max)
+                    products.extend(products_queryset.all())
+            else:
+                for sub_cate in sub_categories:
+                    products.extend(sub_cate.products.all())
         else:
             taget_category = Category.objects.get(id = category_id, slug = category_slug)
             products = Product.objects.filter(category = taget_category)
+            if price_min is not None and price_max is not None:
+                products = products.filter(price__gte=price_min).filter(price__lte=price_max)
 
     else:
         query = request.GET.get('q')
         products = Product.objects.all().order_by('-created_at')
         if query:
             products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            if price_min is not None and price_max is not None:
+                products = products.filter(price__gte=price_min).filter(price__lte=price_max)
 
     paginator = Paginator(products, 12)
     page_number = request.GET.get('page')
@@ -41,7 +68,7 @@ def product_list(request: HttpRequest, category_slug:str = None, category_id: in
     
     return render(
         request, 
-        f'{TEMPLATE_FOLDER_NAME}/product_list.html', 
+        f'{TEMPLATE_FOLDER_NAME}/product/product_list.html', 
         {
             'page_obj': page_obj, 
             'query': query,
@@ -70,7 +97,7 @@ def product_detail(request: HttpRequest, id:int):
 
         return render(
             request, 
-            f'{TEMPLATE_FOLDER_NAME}/product_detail.html', 
+            f'{TEMPLATE_FOLDER_NAME}/product/product_detail.html', 
             {
                 'product': product,
                 'mean_rating': rounded_mean_rating,
